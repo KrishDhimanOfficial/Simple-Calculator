51c37a436a43409dd4bb8d055edc85d8eccad4cb
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-04-29 22:22:06 +0530

    upload project

---
+++ cal.js
@@ -0,0 +1,40 @@
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number");
+const op = document.querySelectorAll(".operator");
+const clear_btn = document.querySelector(".clear");
+const result = document.querySelector(".result");
+
+let num = ' ';
+let operator = ' ';
+display.value = 0;
+
+// getting numbers for calculation
+numbers.forEach(numbers => {
+    numbers.addEventListener("click", () => {
+        display.value = ' ';
+        num = numbers.innerHTML;
+        display.value += num ;
+    });
+});
+
+
+op.forEach(op => {
+    op.addEventListener("click", () => {
+        operator = op.innerHTML;
+        display.value += operator ;``
+    });
+});
+
+// result
+
+result.addEventListener("click", () => {
+    let res = eval(display.value);
+    display.value = res;
+});
+
+// clear input screen 
+clear_btn.addEventListener("click", () => {
+    display.value = 0;
+});
+
+
